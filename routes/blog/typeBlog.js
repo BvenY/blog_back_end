352e6ddafcad6558b04ec3fbc8f913523121215c
Yangbowen <bestbven@163.com>
2019-11-08 19:26:02 +0800

    儿豁 最后一次改

--- routes/blog/typeBlog.js
+++ routes/blog/typeBlog.js
@@ -1,42 +1,51 @@
 let express = require('express');
 let router = express.Router();
-let url = require('url');
-let qs = require('qs');
 const connection = require('../../public/javascripts/database');
 const returnValue = require('../../public/javascripts/return');
 
 
-router.get('/', (req, res) => {
+router.post('/', (req, res) => {
     //先获取get传过来的参数
-    let parseObj = url.parse(req.url);
-    let reqData = qs.parse(parseObj.query);
+    const requestData = req.body;
     //再对用户名进行判空
-    if (!("pageNum" in reqData && "pageSize" in reqData && "blogType" in reqData)) {
+    if (!("typeName" in requestData)) {
         let error = new returnValue.Error(null);
-        error.msg = "参数错误"
+        error.msg = "参数错误";
         return res.json(error);
     }
-    let blogType = reqData.blogType;
-    let pageNum = (reqData.pageNum - 1) * reqData.pageSize;
-    let pageSize = reqData.pageSize * 1;
+    let typeName = requestData.typeName;
     // 定义SQL语句
-    const sqlStr = `select blogID , userID,blogType,blogName,blogDescription,blogTime from blog where blogType = ? LIMIT ?,? `
-    let sqlData = [ blogType,pageNum, pageSize]
+    const sqlStr = `select blogID , userID,blogType,blogName,blogDescription,blogTime from blog where blogType = ?`
+    let sqlData = [typeName ]
     connection.query(sqlStr, sqlData, (err, results) => {
-        let result = JSON.stringify(results);
+        let result = JSON.parse(JSON.stringify(results));
         if (err) {
             let error = new returnValue.Error(err);
             return res.json(error);
         }
-        else if (result === '[]') {
+        else if (JSON.stringify(results) === '[]') {
             let error = new returnValue.Error(null);
             error.msg = "该类型博客为空"
             return res.json(error);
         }
         else {
-            let resData = JSON.parse(result);
-            let success = new returnValue.Success(resData);
-            res.json(success);
+            for (let i = 0; i < result.length; i++) {
+                const userSql = `select userName from user where userID = ?`;
+                let userData = result[i].userID;
+                connection.query(userSql, userData, (err, userReusult) => {
+                    userReusult = JSON.parse(JSON.stringify(userReusult));
+                    if (err) {
+                        let error = new returnValue.Error(err);
+                        return res.json(error);
+                    }
+                    result[i]['userName'] = userReusult[0].userName;
+                    delete result[i]['userID'];
+                });
+            }
+            setTimeout(() => {
+                let success = new returnValue.Success(result);
+                res.json(success);
+            }, 200);
         }
     })
 });
